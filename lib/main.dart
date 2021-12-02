76a17ebec7e7a8e08ba04373cfa17365a61eb611
孟凡旭 <15733225953@163.com>
2021-12-02 17:11:38 +0800

    小卡片

--- lib/main.dart
+++ lib/main.dart
@@ -38,13 +38,56 @@ class PageContent extends StatelessWidget {
         ),
         Container(
           padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-          child: Text(
+          child: const Text(
             '仁王2作为粉丝们期盼已久的续作,和一代相比自然有不少不同之处,今天我们给大家带来了仁王2新内容介绍一览和仁王2新系统详解,感兴趣的小伙伴不要错过了,一起来看看吧。',
             style: TextStyle(
                 fontSize: 14, color: Color.fromRGBO(102, 102, 102, 1)),
             softWrap: true,
           ),
         ),
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: const EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+              border: Border.all(
+                  width: 1, color: const Color.fromRGBO(238, 238, 238, 1))),
+          child: Row(
+            children: [
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: const Text(
+                      '关于武器的选择',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Color.fromRGBO(51, 51, 51, 1),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 4.0),
+                    child: const Text(
+                      '作者：九张',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color.fromRGBO(102, 102, 102, 1),
+                      ),
+                    )
+                  )
+
+                ],
+              )),
+              Icon(
+                Icons.star,
+                color: Colors.red[500],
+              ),
+              const Text('12')
+            ],
+          ),
+        )
       ],
     );
   }
