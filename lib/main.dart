5e60081540767e99bfae4cb05cbdc7cbe895b923
孟凡旭 <15733225953@163.com>
2021-12-02 09:15:31 +0800

    更新列表

--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,80 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+//Stateless widgets 是不可变的, 这意味着它们的属性不能改变 - 所有的值都是最终的.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '欢迎来',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('my app'),
-        ),
-        body: new Center(
-          child: new RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
 
+//Stateful widgets 持有的状态可能在widget生命周期中发生变化.
 class RandomWords extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  createState() => RandomWordsState();
 }
 
+// class RandomWordsState extends State<RandomWords> {
+//   @override
+//   Widget build(BuildContext context) {
+//     final wordPair = new WordPair.random();
+//     return new Text(wordPair.asPascalCase);
+//   }
+// }
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('开始吧'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
   }
 }
