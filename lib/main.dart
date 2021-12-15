2e8a92698c6b02fb6021ebeeaa0e5d2f0ec46270
孟凡旭 <15733225953@163.com>
2021-12-15 17:32:33 +0800

    输入框

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(const MyApp());
 
 //Stateless widgets 是不可变的, 这意味着它们的属性不能改变 - 所有的值都是最终的.
@@ -25,41 +24,27 @@ class MyApp extends StatelessWidget {
 //顶部图片
 class PageContent extends StatelessWidget {
   const PageContent({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         Text(
-            "hello world",
+          "hello world",
           style: TextStyle(
-            color: Colors.red,
-            fontSize: 30.0,
-            height: 1.8,
-            background: Paint()..color=Colors.blue,
-            decoration: TextDecoration.underline,
-            decorationColor: Colors.red,
-            decorationStyle: TextDecorationStyle.dashed
-          ),
+              color: Colors.red,
+              fontSize: 30.0,
+              height: 1.8,
+              background: Paint()..color = Colors.blue,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.red,
+              decorationStyle: TextDecorationStyle.dashed),
           textAlign: TextAlign.end,
         ),
-        const Text.rich(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: "这是个红色的span",
-                style: TextStyle(
-                  color: Colors.red
-                )
-              ),
-              TextSpan(
-                text: "这是个蓝色的span",
-                style: TextStyle(
-                  color: Colors.blue
-                )
-              )
-            ]
-          )
-        ),
+        const Text.rich(TextSpan(children: [
+          TextSpan(text: "这是个红色的span", style: TextStyle(color: Colors.red)),
+          TextSpan(text: "这是个蓝色的span", style: TextStyle(color: Colors.blue))
+        ])),
         ElevatedButton(
           child: const Text('按钮1'),
           onPressed: () {},
@@ -79,23 +64,49 @@ class PageContent extends StatelessWidget {
         ElevatedButton.icon(
           icon: const Icon(Icons.send),
           label: const Text("发送"),
-          onPressed: (){},
+          onPressed: () {},
         ),
         const Image(
           image: AssetImage("assets/images/top1.jpeg"),
           width: 100.0,
         ),
         const Image(
-          image: NetworkImage("https://bkimg.cdn.bcebos.com/pic/d1a20cf431adcbef7d6228b0a9af2edda3cc9f35?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UyMjA=,g_7,xp_5,yp_5/format,f_auto"),
+          image: NetworkImage(
+              "https://bkimg.cdn.bcebos.com/pic/d1a20cf431adcbef7d6228b0a9af2edda3cc9f35?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2UyMjA=,g_7,xp_5,yp_5/format,f_auto"),
           width: 100.0,
           color: Colors.blue,
           colorBlendMode: BlendMode.difference,
         ),
         const Icon(
-          Icons.fingerprint,color: Colors.green,
+          Icons.fingerprint,
+          color: Colors.green,
           size: 80.0,
         ),
-
+        TextField(
+            autofocus: false,
+            textInputAction: TextInputAction.search,
+            decoration: const InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              prefixIcon: Icon(Icons.person),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey)
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue)
+              )
+            ),
+            onChanged: (v) {
+              print("onChange: $v");
+            }),
+        const TextField(
+          autofocus: false,
+          decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+        ),
       ],
     );
   }
